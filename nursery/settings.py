568bb7462b087a5b2f57b509c436d29af3a942a4
Abhijit <supekarabhi7@gmail.com>
2023-02-23 12:37:11 +0530

    setting changed

--- nursery/settings.py
+++ nursery/settings.py
@@ -126,4 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"STATIC"),
+    
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
